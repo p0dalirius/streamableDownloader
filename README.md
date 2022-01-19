9949bb0fc8b3c1c293f7a39ce5cacdb26c496d5c
p0dalirius <podalirius@protonmail.com>
2022-01-19 10:04:20 +0100

    v1

---
+++ README.md
@@ -0,0 +1,30 @@
+# streamableDownloader
+
+## Streamable
+
+
+
+![](./.github/streamable.png)
+
+## The technique
+
+Extract the video URLs from the meta tags for opengraph:
+
+```html
+<meta property="og:url" content="https://streamable.com/abcdef" />
+<meta property="og:video" content="https://cdn-cf-east.streamable.com/video/mp4/abcdef.mp4?Expires=1642074961&Signature=NrOSgEjyOX51sLshgTMtDEShsC97cusGrQKr87hRbJe8NNP8gobxxmqgxaFTakaM5xK6Ykw8K32DLLTbJHO9A5KeGJG2mFvjbYfVPAp07qSd93g6LsesEmqWmnEZHH7MRyAYhq4cYWtQRekFdnsn0JtWvMoAMWI4IUG3nMrkb47tsSYY5XtfYN5KzaTAzh4UrgsyzDVofCVqGYxXR1KpU35hQFtiRan5i0GfFDXfv5YqJ1davybrY3Eygcpk7WJBA6yMtv5uuN6GbWRWvsyVypXFo2kw8NNUbheGgXXHLISaQqbYowMY5NGaX3O1G6uQ7htctIIcDXw13NDggXk4CL__&Key-Pair-Id=WXADY4C7RJIBPIOFRBWM">
+<meta property="og:video:url" content="https://cdn-cf-east.streamable.com/video/mp4/abcdef.mp4?Expires=1642074961&Signature=NrOSgEjyOX51sLshgTMtDEShsC97cusGrQKr87hRbJe8NNP8gobxxmqgxaFTakaM5xK6Ykw8K32DLLTbJHO9A5KeGJG2mFvjbYfVPAp07qSd93g6LsesEmqWmnEZHH7MRyAYhq4cYWtQRekFdnsn0JtWvMoAMWI4IUG3nMrkb47tsSYY5XtfYN5KzaTAzh4UrgsyzDVofCVqGYxXR1KpU35hQFtiRan5i0GfFDXfv5YqJ1davybrY3Eygcpk7WJBA6yMtv5uuN6GbWRWvsyVypXFo2kw8NNUbheGgXXHLISaQqbYowMY5NGaX3O1G6uQ7htctIIcDXw13NDggXk4CL__&Key-Pair-Id=WXADY4C7RJIBPIOFRBWM">
+<meta property="og:video:secure_url" content="https://cdn-cf-east.streamable.com/video/mp4/abcdef.mp4?Expires=1642074961&Signature=NrOSgEjyOX51sLshgTMtDEShsC97cusGrQKr87hRbJe8NNP8gobxxmqgxaFTakaM5xK6Ykw8K32DLLTbJHO9A5KeGJG2mFvjbYfVPAp07qSd93g6LsesEmqWmnEZHH7MRyAYhq4cYWtQRekFdnsn0JtWvMoAMWI4IUG3nMrkb47tsSYY5XtfYN5KzaTAzh4UrgsyzDVofCVqGYxXR1KpU35hQFtiRan5i0GfFDXfv5YqJ1davybrY3Eygcpk7WJBA6yMtv5uuN6GbWRWvsyVypXFo2kw8NNUbheGgXXHLISaQqbYowMY5NGaX3O1G6uQ7htctIIcDXw13NDggXk4CL__&Key-Pair-Id=WXADY4C7RJIBPIOFRBWM">
+```
+
+## Example
+
+```
+bash$ ./streamableDownloader.py -u https://streamable.com/abcdef -o video.mp4
+[>] Downloading to video.mp4 ...
+[>] Downloaded 16.78 MB to video.mp4 ...
+```
+
+## Contributing
+
+Pull requests are welcome. Feel free to open an issue if you want to add other features.
---
+++ streamableDownloader.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : streamableDownloader.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 13 Jan 2022
+
+import os
+import requests
+from bs4 import BeautifulSoup
+import argparse
+
+
+def b_filesize(size):
+    units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB']
+    k = 0
+    for k in range(len(units)):
+        if size < (1024 ** (k + 1)):
+            break
+    return "%4.2f %s" % (round(size / (1024 ** (k)), 2), units[k])
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description="Description message")
+    parser.add_argument("-u", "--url", default=None, required=True, help='Streamable URL')
+    parser.add_argument("-o", "--output-file", default=None, required=False, type=str, help='Output file')
+    parser.add_argument("-v", "--verbose", default=False, action="store_true", help='Verbose mode. (default: False)')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    options = parseArgs()
+
+    r = requests.get(options.url)
+    soup = BeautifulSoup(r.content, "lxml")
+
+    sources = {}
+
+    meta = soup.find('meta', attrs={"property": "og:video"})
+    if meta is not None:
+        sources["og_video"] = meta['content']
+
+    meta = soup.find('meta', attrs={"property": "og:video:url"})
+    if meta is not None:
+        sources["og_video_url"] = meta['content']
+
+    meta = soup.find('meta', attrs={"property": "og:video:secure_url"})
+    if meta is not None:
+        sources["og_video_secure_url"] = meta['content']
+
+    if options.verbose:
+        print("[>] Extracted video urls:")
+        if "og_video" in sources.keys():
+            print("  - og_video: %s" % sources["og_video"])
+        if "og_video_url" in sources.keys():
+            print("  - og_video_url: %s" % sources["og_video_url"])
+        if "og_video_secure_url" in sources.keys():
+            print("  - og_video_secure_url: %s" % sources["og_video_secure_url"])
+
+    for source_name in sources.keys():
+        url = sources[source_name]
+        r = requests.head(url)
+        if r.headers['Content-Type'] in ['video/mp4']:
+
+            total_size = 0
+            if 'Content-Length' in r.headers.keys():
+                total_size = str(r.headers['Content-Length'])
+
+            if options.output_file is not None:
+                filename = options.output_file
+            else:
+                filename = os.path.basename(url).split("?")[0]
+
+            print("[>] Downloading to %s ..." % os.path.basename(filename))
+            total_size = 0
+            with requests.get(url, stream=True) as r:
+                with open(filename, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=16 * 1024):
+                        f.write(chunk)
+                        total_size += len(chunk)
+            print("[>] Downloaded %s to %s ..." % (b_filesize(total_size), os.path.basename(filename)))
+            break
+        else:
+            print("Unknown Content-Type %s for source '%s', skipping" % (r.headers['Content-Type'], source_name))
